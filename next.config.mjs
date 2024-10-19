8682c9913362f8daee7dd111a237ba460dd52880
Tushar7567 <swain.tushar00@gmail.com>
2024-10-19 21:44:37 +0530

    redirect issue in vercel also

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/(.*)",
+        destination: "/",
+      },
+    ];
+  },
+};
 
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
